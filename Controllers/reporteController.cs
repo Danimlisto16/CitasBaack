373c9fb20a6f65f9a027857d0694696e0dc020ed
Daniel Yanez Bravo <Dayanez8@espe.edu.ec>
2020-09-06 12:59:54 -0500

    VERSIONAMIENTO BE

---
+++ Controllers/reporteController.cs
@@ -0,0 +1,29 @@
+﻿using ModelosCitas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CitasBaack.Controllers
+{
+    public class reporteController : ApiController
+    {
+        public static List<getMedicos_Result> List()
+        {
+            using (CitasMedicasEntities1 db = new CitasMedicasEntities1())
+            {
+                try
+                {
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al enviar la lista de MEDICOs " + ex.Message);
+                    throw ex;
+                }
+            }
+        }
+    }
+}
